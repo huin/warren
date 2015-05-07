959866c5869f6327d60ec9e59eedf5dc84ee6f7f
John Beisley <greatred@gmail.com>
2015-05-07 22:43:37 +0100

    Add missing file.

---
+++ util/toml.go
@@ -0,0 +1,13 @@
+package util
+
+import "time"
+
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) UnmarshalText(text []byte) error {
+	var err error
+	d.Duration, err = time.ParseDuration(string(text))
+	return err
+}
